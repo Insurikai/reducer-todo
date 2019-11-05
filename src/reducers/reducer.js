2d3be198a6aa5799bc2ad05e6e2deef0ca96a233
Michael Jordan <Insurikai@gmail.com>
2019-11-05 11:49:10 -0800

    added reducer and default state

---
+++ src/reducers/reducer.js
@@ -0,0 +1,16 @@
+const reducer = (state, action) =>{
+    return 
+}
+const state = [
+    {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+    },
+    {
+        item: 'Learn about reducers',
+        completed: false,
+        id: 3892987589
+    }
+]
+export {reducer, state}
