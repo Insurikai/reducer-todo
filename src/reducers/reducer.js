c394a208b0f4b7a1762489840dbb5ecc78ae0fd2
Michael Jordan <Insurikai@gmail.com>
2019-11-05 12:38:42 -0800

    added the logic for the reducer

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,16 +1,23 @@
 const reducer = (state, action) =>{
+    switch(action.type){
+        case "Add":
+            return [...state, action.payload]
+        case "Clear Completed":
+            return state.filter(todoItem => !todoItem.completed)
+        default:
+            return state;
+    }
     return 
 }
-const state = [
-    {
-    item: 'Learn about reducers',
-    completed: false,
-    id: 3892987589
-    },
+const data = [
     {
         item: 'Learn about reducers',
         completed: false,
         id: 3892987589
+    },
+    {
+        item: 'item2',
+        completed: false,
+        id: 3457834
     }
 ]
-export {reducer, state}
