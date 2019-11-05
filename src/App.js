a46755c7784a100b14cfb9b7d9ecd85ed14211c7
Michael Jordan <Insurikai@gmail.com>
2019-11-05 12:03:23 -0800

    wrapped components in context and imported reducer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
+
+//Components
 import TodoList from './components/todoList';
 import ClearTodo from "./components/clearTodo";
 import AddTodoForm from "./components/addTodoForm";
+//Contexts
+import TodoContext from "./contexts/TodoContext";
+//Reducers
+import {reducer, state} from "./reducers/reducer";
 
 function App() {
+  
   return (
     <div className="App">
-      <TodoList/>
-      <AddTodoForm/>
-      <ClearTodo/>
+      <TodoContext.Provider value="">
+        <TodoList/>
+        <AddTodoForm/>
+        <ClearTodo/>
+      </TodoContext.Provider>
     </div>
   );
 }
